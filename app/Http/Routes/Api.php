e6fa52899cc0d338dd858ef8b92aa085b0466178
Matt Bannon <matt@ttam.org>
2018-03-26 00:18:28 +1100

    Added routes for list members

--- app/Http/Routes/Api.php
+++ app/Http/Routes/Api.php
@@ -11,5 +11,11 @@ $router->group(['prefix' => 'mailchimp', 'namespace' => 'MailChimp'], function (
         $router->get('/{listId}', 'ListsController@show');
         $router->put('/{listId}', 'ListsController@update');
         $router->delete('/{listId}', 'ListsController@remove');
+
+        $router->post('/{listId}/members', 'ListsController@createMember');
+        $router->get('/{listId}/members', 'ListsController@showMembers');
+        $router->put('/{listId}/members/{memberId}', 'ListsController@updateMember');
+        $router->delete('/{listId}/members/{memberId}', 'ListsController@removeMember');
     });
+
 });
